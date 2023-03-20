640bc7a62b60e87e6e7dc0bd9f46d150757e660f
Canceer14 <jobaamarkevich@gmail.com>
2023-03-20 12:56:52 +0300

    init: start (20.03.2023 12:56)

---
+++ js/app.js
@@ -0,0 +1,14 @@
+// for (let i = 1; i <= 10; i++) {
+//   if (i % 2 === 0) {
+//     alert(i);
+//   }
+// }   ЦИКЛ ЧЕТНЫЕ
+
+// for (let i = 0; i < 3; i++) {
+//   alert( `number ${i}!` );
+// }
+
+let i = 0;
+while (i < 3) {
+  alert(`nember ${i++}`);
+}
